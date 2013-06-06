7ece6ce9ab0ff3654914c5e31a2e4af0f9c48479
andrewmc2 <acl.mccallum2@gmail.com>
2013-06-06 11:00:37 -0500

    first commit

---
+++ SushiSnob/SushiSnob/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SushiSnob
+//
+//  Created by Andrew McCallum14 on 2013-06-06.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SushiSnob/SushiSnob/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SushiSnob
+//
+//  Created by Andrew McCallum14 on 2013-06-06.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

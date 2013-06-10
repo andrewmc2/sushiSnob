e6261fb8ed65662b21d67e3be89c9aeffe502816
Craig Pellegrini <craig.pellegrini@live.com>
2013-06-10 09:40:56 -0500

    added sushi venue VC

---
+++ SushiSnob/SushiSnob/AddSushiVenueVC.h
@@ -0,0 +1,13 @@
+//
+//  AddSushiVenueVC.h
+//  SushiSnob
+//
+//  Created by Craig on 6/9/13.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSushiVenueVC : UIViewController
+
+@end
---
+++ SushiSnob/SushiSnob/AddSushiVenueVC.m
@@ -0,0 +1,38 @@
+//
+//  AddSushiVenueVC.m
+//  SushiSnob
+//
+//  Created by Craig on 6/9/13.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import "AddSushiVenueVC.h"
+
+@interface AddSushiVenueVC ()
+
+@end
+
+@implementation AddSushiVenueVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

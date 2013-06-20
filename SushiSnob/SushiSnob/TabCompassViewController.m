4cb2a95f8e77815f506e8b16b5ecdc474a61fe6c
andrewmc2 <acl.mccallum2@gmail.com>
2013-06-20 11:15:02 -0500

    SS1.7 no activity incatator map title

--- SushiSnob/SushiSnob/TabCompassViewController.m
+++ SushiSnob/SushiSnob/TabCompassViewController.m
@@ -91,8 +91,8 @@
     self.sadSushiImage.alpha = 0;
     [self setupCompassObjectsAndLabels];
     [self startStandardLocationServices];
-    [self.activityIndicator startAnimating];
-    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+    //[self.activityIndicator startAnimating];
+    //[[UIApplication sharedApplication] beginIgnoringInteractionEvents];
     UITapGestureRecognizer *singleTapWebview = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(webViewTapAreaTapped:)];
         [self.webviewTapArea addGestureRecognizer:singleTapWebview];
     }
@@ -220,8 +220,8 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
     
-    [self.activityIndicator stopAnimating];
-    [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+    //[self.activityIndicator stopAnimating];
+    //[[UIApplication sharedApplication] endIgnoringInteractionEvents];
 
     if (!self.headingDidStartUpdating) {
         [self setupCompassObjectsAndLabels];
